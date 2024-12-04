0b0fe1700a7c990487ba2ea2d93a1d108c5fc696
Hasnain081992 <Hasnain081992@gmail.com>
2024-12-04 23:38:17 +0000

    postgrefinal

--- sql/sqlsample.py
+++
@@ -1,9 +0,0 @@
-
-import mysql.connector
-con = mysql.connector.connect(
-    user = 'root',
-    host = 'localhost',
-    database = 'challange1',
-    password = 'root'
-)
-print(con)
--- test/practice/test_practice.py
+++
@@ -1,2 +0,0 @@
-def testlogin():
-    print('login successfully')
